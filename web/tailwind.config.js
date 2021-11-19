1510deadb870a68d5078e34072cc35901713b047
Connor Davis <19422@kloofhigh.co.za>
2021-11-19 14:14:11 +0200

    Add tailwind and update readme

---
+++ web/tailwind.config.js
@@ -0,0 +1,85 @@
+let defaultTheme = require('tailwindcss/defaultTheme');
+let colors = require('tailwindcss/colors');
+
+module.exports = {
+  darkMode: 'class',
+  mode: 'jit',
+  purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        gray: colors.trueGray,
+      },
+      fontWeight: ['hover', 'focus'],
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      boxShadow: {
+        sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
+        DEFAULT:
+          '0 1px 4px 0 rgba(0, 0, 0, 0.09), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
+        md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+        lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+        xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
+        '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
+        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+        inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
+        none: 'none',
+      },
+      transitionProperty: {
+        width: 'width',
+        height: 'height'
+      },
+      keyframes: {
+        'fade-in-down': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-20px)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)'
+          },
+        },
+        'fade-out-down': {
+          'from': {
+            opacity: '1',
+            transform: 'translateY(0px)'
+          },
+          'to': {
+            opacity: '0',
+            transform: 'translateY(10px)'
+          },
+        },
+        'fade-in-up': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(10px)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)'
+          },
+        },
+        'fade-out-up': {
+          'from': {
+            opacity: '1',
+            transform: 'translateY(0px)'
+          },
+          'to': {
+            opacity: '0',
+            transform: 'translateY(-20px)'
+          },
+        }
+      },
+      animation: {
+        'fade-in-down': 'fade-in-down 0.5s ease-out',
+        'fade-out-down': 'fade-out-down 0.5s ease-out',
+        'fade-in-up': 'fade-in-up 0.5s ease-out',
+        'fade-out-up': 'fade-out-up 0.5s ease-out'
+      }
+    },
+  },
+  variants: {},
+  plugins: []
+};
