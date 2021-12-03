79d35c5cf518efcc3595d1c09fdfc8f470f0ae4d
Connor Davis <19422@kloofhigh.co.za>
2021-12-03 14:12:03 +0200

    Updates

--- web/src/pages/tabs/friendsTab.jsx
+++ web/src/pages/tabs/friendsTab.jsx
@@ -113,7 +113,22 @@ let FriendsTabPage = () => {
                   </div>
 
                   <div class="flex space-x-2">
-                    <div class="flex flex-col justify-center items-center p-2 border-l border-t border-r border-b border-gray-300 dark:border-gray-800 cursor-pointer rounded-full"></div>
+                    <div class="flex flex-col justify-center items-center p-2 border-l border-t border-r border-b border-gray-300 dark:border-gray-800 hover:text-gray-400 cursor-pointer rounded-full">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="h-5 w-5"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
+                        />
+                      </svg>
+                    </div>
                   </div>
                 </div>
               );
