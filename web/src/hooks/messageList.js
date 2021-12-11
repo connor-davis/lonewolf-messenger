4bc486638eff92cb93536c1874d6f64a4d51590f
Connor Davis <19422@kloofhigh.co.za>
2021-12-11 16:52:43 +0200

    Successful infinite scroll test with sessionStorage #fast

--- web/src/hooks/messageList.js
+++ web/src/hooks/messageList.js
@@ -3,67 +3,105 @@ import { onMount } from 'solid-js';
 import { createStore } from 'solid-js/store';
 
 let useMessageList = (roomId, pub) => {
-  let [state, setState] = createStore([]);
+  let [state, setState] = createStore([], { name: `${roomId}-messages` });
 
   onMount(async () => {
     let userPair = await gun.user()._.sea;
     let friend = await gun.user(pub);
 
-    gun
-      .user()
-      .get('messages')
-      .get(roomId)
-      .once((messages) => {
-        (async () => {
-          let initial = [];
+    if (sessionStorage.getItem(`${roomId}-measages`) && state.length === 0) {
+      setState(JSON.parse(sessionStorage.getItem(`${roomId}-measages`)));
 
-          for (let key in messages) {
-            let message = messages[key].toString();
+      return (async () => {
+        let messagesDiv = document.querySelector('#messages');
 
-            let decryptSecretFriend = await SEA.secret(friend.epub, userPair);
-            let decryptedMessageFriend = await SEA.decrypt(
-              message,
-              decryptSecretFriend
-            );
+        if (messagesDiv) {
+          messagesDiv.scrollTop =
+            messagesDiv.scrollHeight - messagesDiv.clientHeight;
+        }
 
-            if (decryptedMessageFriend) {
+        gun
+          .user()
+          .get('messages')
+          .get(roomId)
+          .map()
+          .once(async (message) => {
+            if (message.toString().startsWith('SEA')) {
+              let decryptSecretFriend = await SEA.secret(friend.epub, userPair);
+              let decryptedMessageFriend = await SEA.decrypt(
+                message,
+                decryptSecretFriend
+              );
+
+              if (decryptedMessageFriend) {
+                let individual = {
+                  ...decryptedMessageFriend,
+                  encrypted: true,
+                };
+
+                let exists =
+                  state.filter((current) => current.id === individual.id)[0] !==
+                  undefined;
+
+                if (!exists) {
+                  setState(
+                    [...state, individual].sort((a, b) => {
+                      if (a.timeSent > b.timeSent) return 1;
+                      if (a.timeSent < b.timeSent) return -1;
+                      return 0;
+                    })
+                  );
+
+                  sessionStorage.setItem(
+                    `${roomId}-measages`,
+                    JSON.stringify(state)
+                  );
+
+                  return scroll();
+                }
+              }
+            } else {
               let individual = {
-                ...decryptedMessageFriend,
-                encrypted: true,
+                ...message,
+                encrypted: false,
               };
 
               let exists =
                 state.filter((current) => current.id === individual.id)[0] !==
                 undefined;
 
-              if (!exists) initial.push(individual);
+              if (!exists) {
+                setState(
+                  [...state, individual].sort((a, b) => {
+                    if (a.timeSent > b.timeSent) return 1;
+                    if (a.timeSent < b.timeSent) return -1;
+                    return 0;
+                  })
+                );
+
+                sessionStorage.setItem(
+                  `${roomId}-measages`,
+                  JSON.stringify(state)
+                );
+
+                return scroll();
+              }
             }
-          }
+          });
+      })();
+    } else {
+      gun
+        .user()
+        .get('messages')
+        .get(roomId)
+        .once((messages) => {
+          try {
+            (async () => {
+              let initial = [];
 
-          let messagesDiv = document.querySelector('#messages');
+              for (let key in messages) {
+                let message = messages[key].toString();
 
-          if (messagesDiv) {
-            messagesDiv.scrollTop =
-              messagesDiv.scrollHeight - messagesDiv.clientHeight;
-          }
-
-          setState(
-            initial.sort((a, b) => {
-              if (a.timeSent > b.timeSent) return 1;
-              if (a.timeSent < b.timeSent) return -1;
-              return 0;
-            })
-          );
-
-          scroll();
-
-          gun
-            .user()
-            .get('messages')
-            .get(roomId)
-            .map()
-            .once(async (message) => {
-              if (message.toString().startsWith('SEA')) {
                 let decryptSecretFriend = await SEA.secret(
                   friend.epub,
                   userPair
@@ -84,21 +122,109 @@ let useMessageList = (roomId, pub) => {
                       (current) => current.id === individual.id
                     )[0] !== undefined;
 
-                  if (!exists) {
-                    setState(
-                      [...state, individual].sort((a, b) => {
-                        if (a.timeSent > b.timeSent) return 1;
-                        if (a.timeSent < b.timeSent) return -1;
-                        return 0;
-                      })
-                    );
-                    return scroll();
-                  }
+                  if (!exists) initial.push(individual);
                 }
               }
-            });
-        })();
-      });
+
+              let messagesDiv = document.querySelector('#messages');
+
+              if (messagesDiv) {
+                messagesDiv.scrollTop =
+                  messagesDiv.scrollHeight - messagesDiv.clientHeight;
+              }
+
+              setState(
+                initial.sort((a, b) => {
+                  if (a.timeSent > b.timeSent) return 1;
+                  if (a.timeSent < b.timeSent) return -1;
+                  return 0;
+                })
+              );
+
+              scroll();
+
+              sessionStorage.setItem(
+                `${roomId}-measages`,
+                JSON.stringify(state)
+              );
+
+              gun
+                .user()
+                .get('messages')
+                .get(roomId)
+                .map()
+                .once(async (message) => {
+                  if (message.toString().startsWith('SEA')) {
+                    let decryptSecretFriend = await SEA.secret(
+                      friend.epub,
+                      userPair
+                    );
+                    let decryptedMessageFriend = await SEA.decrypt(
+                      message,
+                      decryptSecretFriend
+                    );
+
+                    if (decryptedMessageFriend) {
+                      let individual = {
+                        ...decryptedMessageFriend,
+                        encrypted: true,
+                      };
+
+                      let exists =
+                        state.filter(
+                          (current) => current.id === individual.id
+                        )[0] !== undefined;
+
+                      if (!exists) {
+                        setState(
+                          [...state, individual].sort((a, b) => {
+                            if (a.timeSent > b.timeSent) return 1;
+                            if (a.timeSent < b.timeSent) return -1;
+                            return 0;
+                          })
+                        );
+
+                        sessionStorage.setItem(
+                          `${roomId}-measages`,
+                          JSON.stringify(state)
+                        );
+
+                        return scroll();
+                      }
+                    }
+                  } else {
+                    let individual = {
+                      ...message,
+                      encrypted: false,
+                    };
+
+                    let exists =
+                      state.filter(
+                        (current) => current.id === individual.id
+                      )[0] !== undefined;
+
+                    if (!exists) {
+                      setState(
+                        [...state, individual].sort((a, b) => {
+                          if (a.timeSent > b.timeSent) return 1;
+                          if (a.timeSent < b.timeSent) return -1;
+                          return 0;
+                        })
+                      );
+
+                      sessionStorage.setItem(
+                        `${roomId}-measages`,
+                        JSON.stringify(state)
+                      );
+
+                      return scroll();
+                    }
+                  }
+                });
+            })();
+          } catch (err) {}
+        });
+    }
   });
 
   let scroll = () => {
